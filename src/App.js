ade36bcefeed4779a035193851bcd7caf25dcd81
Shadid <62571805+Shadid516@users.noreply.github.com>
2024-07-06 22:09:39 +0300

    Change file structure for compliance with project requirements, updated imports and verified functionality too

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import AddTodo from './components/AddTodo';
+import Home from './pages/Home';
+import AddTodo from './pages/AddTodo';
 import { TodoProvider } from './context/TodoContext';
+import Navbar from './components/navbar';
 // TODO ask why 'app' is given as an arrow function
 const App = () => {
   return (
     <TodoProvider>
       <Router>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/add-todo">Add Todo</Link></li>
-          </ul>
-        </nav>
+        <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/add-todo" element={<AddTodo />} />
--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -3,8 +3,8 @@ import React, { createContext, useState } from 'react';
 export const TodoContext = createContext();
 
 export const TodoProvider = ({ children }) => {
-    const [todos, setTodos] = useState(['Do the dishes','Take out the trash']);
-// TODO-Add Comment
+    const [todos, setTodos] = useState(['Do the dishes', 'Take out the trash']);
+    // TODO Merge context logic into todo page
     return (
         <TodoContext.Provider value={{ todos, setTodos }}>
             {children}
